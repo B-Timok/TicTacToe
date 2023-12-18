7d12eff147c688dd40384c9917152f2876484ce0
Brandon Timok <99372902+B-Timok@users.noreply.github.com>
2023-12-17 20:12:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,26 @@ boxes.forEach( box => {
     })
 })
 
+// Function to check for a possible winning move for the computer
+function checkForWinningMove(turn) {
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+        [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    for (let i = 0; i<winConditions.length; i++) {
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if (v0 === turn && v0 === v1 && v0 === v2) {
+            return true;
+        }
+    };
+    return false;
+}
+
 let thinkingIndicator = document.getElementById("thinking-indicator");
 // Function to make the computer move
 function computerMove() {
@@ -32,6 +52,50 @@ function computerMove() {
     setTimeout(() => {
         let emptyBoxes = Array.from(boxes).filter(box => box.innerHTML === "");
         if (emptyBoxes.length > 0) {
+            // Check if there is a winning move for the player
+            let userWinningMove = false;
+            for (let i = 0; i<emptyBoxes.length; i++) {
+                emptyBoxes[i].innerHTML = "X";
+                if (checkForWinningMove("X")) {
+                    emptyBoxes[i].innerHTML = turn;
+                    emptyBoxes[i].classList.add("computer");
+                    emptyBoxes[i].style.color = "rgb(33, 154, 187)";
+                    checkWin();
+                    checkDraw();
+                    changeTurn();
+                    userWinningMove = true;
+                    break;
+                }
+                emptyBoxes[i].innerHTML = "";
+            }
+            // If a winning move was found for the user, return from the function
+            if (userWinningMove) {
+                thinkingIndicator.innerHTML = "";
+                isComputerTurn = false;
+                return;
+            }
+
+            // Check if there is a winning move for the computer
+            let winningMove = false;
+            for (let i = 0; i<emptyBoxes.length; i++) {
+                emptyBoxes[i].innerHTML = "O";
+                if (checkForWinningMove("O")) {
+                    emptyBoxes[i].classList.add("computer");
+                    emptyBoxes[i].style.color = "rgb(33, 154, 187)";
+                    checkWin();
+                    checkDraw();
+                    changeTurn();
+                    winningMove = true;
+                    break;
+                }
+                emptyBoxes[i].innerHTML = "";
+            }
+            // If a winning move was found, return from the function
+            if (winningMove) {
+                thinkingIndicator.innerHTML = "";
+                isComputerTurn = false;
+                return;
+            }
             let randomBox = emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
             randomBox.innerHTML = turn;
             randomBox.classList.add("computer");
@@ -42,7 +106,7 @@ function computerMove() {
         }
         thinkingIndicator.innerHTML = "";
         isComputerTurn = false;
-    }, 1300); // 1000 milliseconds = 1 second
+    }, 1000); // 1000 milliseconds = 1 second
 }
 
 // Function to change the turn
@@ -79,7 +143,7 @@ function checkWin() {
                 boxes[winConditions[i][j]].style.color = "#000"
             }
         }
-    };
+    }
 }
 
 // Function to check if the game is a draw
